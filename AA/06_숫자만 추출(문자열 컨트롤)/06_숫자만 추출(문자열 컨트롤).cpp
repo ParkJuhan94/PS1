efdfb9b997038cf7a5b69081d5dbafcf4c500a61
JuHan <1004sunye@naver.com>
2022-01-27 19:58:52 +0900

    20220127-01

---
+++ AA/06_숫자만 추출(문자열 컨트롤)/06_숫자만 추출(문자열 컨트롤).cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	char a[100];
+	int res = 0, cnt = 0, i;
+	scanf("%s", &a);
+	for (i = 0; a[i] != '\0'; i++) {
+		if (a[i] >= 48 && a[i] <= 57) {
+			res = res * 10 + (a[i] - 48);
+		}
+	}
+	printf("%d\n", res);
+	for (i = 1; i <= res; i++) {
+		if (res % i == 0) cnt++;
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
+
