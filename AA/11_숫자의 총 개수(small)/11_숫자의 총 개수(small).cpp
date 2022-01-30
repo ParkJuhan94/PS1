5c03887d876e5a632217c4c447ea36174cc7d6a8
JuHan <1004sunye@naver.com>
2022-01-30 14:09:02 +0900

    20220130-02

--- AA/11_숫자의 총 개수(small)/11_숫자의 총 개수(small).cpp
+++ AA/11_숫자의 총 개수(small)/11_숫자의 총 개수(small).cpp
@@ -1,3 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 using namespace std;
+
+int numDigit(int x) {
+	int cnt = 0;
+
+	while (x > 0) {
+		x /= 10;
+		cnt++;
+	}
+
+	return cnt;
+}
+
+int main() {
+	int n,i, sum=0;
+	scanf("%d", &n); 
+
+	for (i = 1; i <= n; i++) {
+		sum += numDigit(i);
+	}
+	printf("%d\n", sum);
+}
