d9b7a187bafcb2ec74f6d0123c03969a865fba97
JuHan <1004sunye@naver.com>
2022-04-17 20:08:00 +0900

    0417

--- AA/20_가위 바위 보(기본코딩설계)/20_가위 바위 보(기본코딩설계).cpp
+++ AA/20_가위 바위 보(기본코딩설계)/20_가위 바위 보(기본코딩설계).cpp
@@ -2,6 +2,57 @@
 #include <stdio.h>
 #include <algorithm>
 
-int main() {
+void printWinner(int a, int b) {
+	if (a == 1) {
+		if (b == 1) {
+			printf("D\n");
+		}
+		else if (b == 2) {
+			printf("B\n");
+		}
+		else if (b == 3) {
+			printf("A\n");
+		}
+	}
+	else if (a == 2) {
+		if (b == 1) {
+			printf("A\n");
+		}
+		else if (b == 2) {
+			printf("D\n");
+		}
+		else if (b == 3) {
+			printf("B\n");
+		}
+	}
+	else if (a == 3) {
+		if (b == 1) {
+			printf("B\n");
+		}
+		else if (b == 2) {
+			printf("A\n");
+		}
+		else if (b == 3) {
+			printf("D\n");
+		}
+	}
+}
 
+int main() {
+	int n;
+	int a[100];
+	int b[100];
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);		
+	}
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &b[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		printWinner(a[i], b[i]);
+	}
 }
