d174a4fcf5494ad3942627472d61b0c05d8076b4
JuHan <1004sunye@naver.com>
2022-05-01 19:33:36 +0900

    0501

--- AA/29_3의 개수는 (large 제한시간 1초)/29_3의 개수는 (large 제한시간 1초).cpp
+++ AA/29_3의 개수는 (large 제한시간 1초)/29_3의 개수는 (large 제한시간 1초).cpp
@@ -3,3 +3,24 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int n, left = 77, right, cur, k = 1, res = 0;
+	scanf("%d", &n);
+	while (left != 0) {
+		left = n / (k * 10);
+		right = n % k;
+		cur = (n / k) % 10;
+		if (3 < cur) {
+			res = res + ((left + 1) * k);
+		}
+		else if (3 == cur) {
+			res = res + ((left * k) + (right + 1));
+		}
+		else res = res + (left * k);
+		k = k * 10;
+	}
+	printf("%d\n", res);
+	return 0;
+}
--- AA/30_3의 개수는 (small 구글 인터뷰)/30_3의 개수는 (small 구글 인터뷰).cpp
+++ AA/30_3의 개수는 (small 구글 인터뷰)/30_3의 개수는 (small 구글 인터뷰).cpp
@@ -3,3 +3,19 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
+
+int main() {
+	//freopen("input.txt", "rt", stdin);
+	int n, tmp, i, cnt = 0, digit;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		tmp = i;
+		while (tmp > 0) {
+			digit = tmp % 10;
+			if (digit == 3) cnt++;
+			tmp = tmp / 10;
+		}
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
